de593c49c556ce6f5f76e0576677c791e97eac64
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-08 20:36:53 -0300

    Requisito 6: Testa se os retornos para os parâmetro location e popularity estão corretos

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -5,4 +5,18 @@ describe('Testes da função handlerElephants', () => {
     expect(handlerElephants).not.toBeUndefined();
     expect(handlerElephants).toBeInstanceOf(Function);
   });
+
+  it('Teste: Se retorna undefined ao não ser passado parâmetro algum', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('Teste: Se retorna a string "NW" quando for passada a string "location" como parâmetro', () => {
+    expect(typeof handlerElephants('location')).toBe('string');
+    expect(handlerElephants('location')).toBe('NW');
+  });
+
+  it('Teste: Se retorna a string "NW" quando for passada a string "location" como parâmetro', () => {
+    expect(typeof handlerElephants('popularity')).toBe('number');
+    expect(handlerElephants('popularity')).toBe(5);
+  });
 });
