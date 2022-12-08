e541fcdca46a41550a4a0ad42af871a350b3ce20
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-08 16:47:47 -0300

    Requisito 4: Implementa a função isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => employees.reduce((managers, currEmplo) => (
+  [...managers, ...currEmplo.managers]
+), []).includes(id);
+
+// const isManager = (id) => {
+//   return employees.reduce((managers, currEmplo) => {
+//     return managers.concat(...currEmplo.managers);
+//   }, []).includes(id);
+// };
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
