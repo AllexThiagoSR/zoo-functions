2d23be503e90b8861f00c7537a6b26d97746901b
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-08 20:57:20 -0300

    Requisito 6: Testa se retorna os valores certos para os argumentos averageAge e availability

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -15,8 +15,18 @@ describe('Testes da função handlerElephants', () => {
     expect(handlerElephants('location')).toBe('NW');
   });
 
-  it('Teste: Se retorna a string "NW" quando for passada a string "location" como parâmetro', () => {
+  it('Teste: Se retorna 5 quando for passada a string "popularity" como parâmetro', () => {
     expect(typeof handlerElephants('popularity')).toBe('number');
     expect(handlerElephants('popularity')).toBe(5);
+  });
+
+  it('Teste: Se retorna um array com dias da semana quando for passada a string "availability" como parâmetro', () => {
+    expect(Array.isArray(handlerElephants('availability'))).toBe(true);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+
+  it('Teste: Se retorna 10.5 com dias da semana quando for passada a string "averageAge" como parâmetro', () => {
+    expect(typeof handlerElephants('averageAge')).toBe('number');
+    expect(handlerElephants('averageAge')).toBe(10.5);
   });
 });
