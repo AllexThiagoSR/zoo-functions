541e5e7f1741199f05b7aa9f5b12a5db82890203
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-08 19:50:52 -0300

    Requisito 6: Testa se handlerElephants realmente existe e é uma função

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,8 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função handlerElephants', () => {
+  it('Teste: Se handlerElephants existe e é uma função', () => {
+    expect(handlerElephants).not.toBeUndefined();
+    expect(handlerElephants).toBeInstanceOf(Function);
+  });
+});
