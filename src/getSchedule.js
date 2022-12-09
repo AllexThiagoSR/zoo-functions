2534bf001a1bfd02404cfb7fd31f4d7fa977f368
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-09 10:00:15 -0300

    Requisito 9: Resolução semi-final

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+const { species, hours } = data;
+
+const getSchedule = (target) => {
+  if (typeof target === 'string') return species.find(({ name }) => name === target).availability;
+  return Object.keys(hours).reduce((days, currDay) => {
+    const auxi = { ...days };
+    auxi[currDay] = {};
+    auxi[currDay].officeHour = `Open from ${hours[currDay].open}am until ${hours[currDay].close}pm`;
+    auxi[currDay].exhibition = species.filter(({ availability: av }) => av.includes(currDay)).map(({ name }) => name);
+    return auxi;
+  }, {});
 };
 
+console.log(getSchedule());
 module.exports = getSchedule;
