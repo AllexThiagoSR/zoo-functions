499d1013d8070a7c6c28015ad3b22ab22deff101
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-09 10:36:59 -0300

    Requisito 9: Implementa a função getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,16 +2,37 @@ const data = require('../data/zoo_data');
 
 const { species, hours } = data;
 
+const closedDays = ['Monday'];
+
+const filterSpeciesExhibition = (day) => species.filter(({ availability: av }) => av.includes(day));
+
+const mapSpeciesExhibitionNames = (speciesOfDay) => speciesOfDay.map(({ name }) => name);
+
+const creatDaySchedule = (day) => ({
+  officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+  exhibition: mapSpeciesExhibitionNames(filterSpeciesExhibition(day)),
+});
+
+const creatClosedDay = (day) => ({
+  officeHour: 'CLOSED',
+  exhibition: 'The zoo will be closed!',
+});
+
+const checkAnimal = (animal) => species.find(({ name }) => name === animal);
+
+const isClosed = (day) => (!closedDays.includes(day) ? creatDaySchedule(day) : creatClosedDay(day));
+
 const getSchedule = (target) => {
-  if (typeof target === 'string') return species.find(({ name }) => name === target).availability;
-  return Object.keys(hours).reduce((days, currDay) => {
-    const auxi = { ...days };
-    auxi[currDay] = {};
-    auxi[currDay].officeHour = `Open from ${hours[currDay].open}am until ${hours[currDay].close}pm`;
-    auxi[currDay].exhibition = species.filter(({ availability: av }) => av.includes(currDay)).map(({ name }) => name);
+  const days = Object.keys(hours);
+  const animal = checkAnimal(target);
+  if (animal !== undefined) return animal.availability;
+  if (days.includes(target)) return { [target]: isClosed(target) };
+  return days.reduce((schedule, currDay) => {
+    const auxi = { ...schedule };
+    auxi[currDay] = isClosed(currDay);
     return auxi;
   }, {});
 };
 
-console.log(getSchedule());
+// console.log(getSchedule('day'));
 module.exports = getSchedule;
