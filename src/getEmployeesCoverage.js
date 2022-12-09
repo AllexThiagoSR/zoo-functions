fcd10f04841b1d427ff7b7753ffa9d2835ff07ae
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-09 15:14:30 -0300

    Requisito 11: Implementa a função getEmployeesCoverage e 6 funções auxiliares

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const { employees, species } = data;
+
+const getEmployee = (info) => {
+  if (!info) {
+    return null;
+  }
+  const { name = '', id: idRec = '' } = info;
+  return employees.find(({ id, lastName, firstName }) =>
+    (id === idRec || lastName === name || firstName === name));
+};
+
+const findSpecie = (specieID) => species.find(({ id }) => specieID === id);
+
+const getLocations = (speciesID) => speciesID.map((id) => findSpecie(id).location);
+
+const getSpecies = (speciesID) => speciesID.map((id) => findSpecie(id).name);
+
+const creatCoverage = ({ firstName, lastName, responsibleFor, id }) => ({
+  id,
+  fullName: `${firstName} ${lastName}`,
+  species: getSpecies(responsibleFor),
+  locations: getLocations(responsibleFor),
+});
+
+const coverageAllEmployees = () => employees.map((employee) => creatCoverage(employee));
+
+const getEmployeesCoverage = (info) => {
+  const employee = getEmployee(info);
+  if (employee === undefined) throw new Error('Informações inválidas');
+  return (employee === null) ? coverageAllEmployees() : creatCoverage(employee);
 };
 
 module.exports = getEmployeesCoverage;
