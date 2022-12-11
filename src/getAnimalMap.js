9d1d3a0143d922eeb4e44dbee41b56cc2572793c
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-11 16:41:53 -0300

    Requisito 14: Função retorna um mapa com as espécies separadas por região ao não receber nenhum parâmetro ou um parâmetro inválido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const getLocations = () =>
+  species.reduce((locations, { location }) =>
+    (!locations.includes(location) ? [...locations, location] : locations), []);
+
+const filterByLocation = (loc) => species.filter(({ location }) => location === loc);
+
+const getSpeciesNames = (speciesObj) => speciesObj.map(({ name }) => name);
+
+const nullMapMaker = (locations) =>
+  locations.reduce((finalMap, currLoc) =>
+    ({ ...finalMap, [currLoc]: getSpeciesNames(filterByLocation(currLoc)) }), []);
+
+const isObject = (obj) => typeof obj === 'object' && !Array.isArray(obj);
+
 const getAnimalMap = (options) => {
-  // seu código aqui
+  if (!isObject(options)) return nullMapMaker(getLocations());
 };
 
+console.log(getAnimalMap({}));
 module.exports = getAnimalMap;
