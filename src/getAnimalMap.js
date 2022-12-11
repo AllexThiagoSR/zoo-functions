dbb597ac803fd125c880ab8520ce789a0a99030e
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-11 17:54:22 -0300

    Requisito 14: Retorna os nomes dos residentes de cada espécie ordenados ou não

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,23 +2,41 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
+const sortedOrNo = (thingToSort, sort) =>
+  (sort === true ? thingToSort.sort() : thingToSort);
+
 const getLocations = () =>
   species.reduce((locations, { location }) =>
     (!locations.includes(location) ? [...locations, location] : locations), []);
 
 const filterByLocation = (loc) => species.filter(({ location }) => location === loc);
 
-const getSpeciesNames = (speciesObj) => speciesObj.map(({ name }) => name);
+const getNames = (speciesObj, sorted) => sortedOrNo(speciesObj.map(({ name }) => name), sorted);
 
 const nullMapMaker = (locations) =>
   locations.reduce((finalMap, currLoc) =>
-    ({ ...finalMap, [currLoc]: getSpeciesNames(filterByLocation(currLoc)) }), []);
+    ({ ...finalMap, [currLoc]: getNames(filterByLocation(currLoc)) }), {});
+
+const getResidentsNames = (speciesOfLoc, sorted) =>
+  speciesOfLoc.reduce((acc, { name, residents }) =>
+    [...acc, { [name]: getNames(residents, sorted) }], []);
+
+const mapMakerWNames = (locations, sorted) =>
+  locations.reduce((finalMap, currLoc) =>
+    ({ ...finalMap, [currLoc]: getResidentsNames(filterByLocation(currLoc), sorted) }), {});
 
 const isObject = (obj) => typeof obj === 'object' && !Array.isArray(obj);
 
-const getAnimalMap = (options) => {
-  if (!isObject(options)) return nullMapMaker(getLocations());
+const checkPath = ({ includesName, sex, sorted }) => {
+  if (includesName) return mapMakerWNames(getLocations(), sorted);
+  return nullMapMaker(getLocations());
 };
 
-console.log(getAnimalMap({}));
+const getAnimalMap = (options) => {
+  if (!isObject(options)) return nullMapMaker(getLocations());
+  // const { includesName, sorted, sex } = options;
+  return checkPath(options);
+};
+
+// console.log(getAnimalMap({ includesName: true, sorted: true }).NE);
 module.exports = getAnimalMap;
