8dccd4e9c6534107623f9060f9ffc2be0736fed0
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-12 17:07:06 -0300

    Requisito 2: Deixando tudo em uma linha

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,9 +2,7 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const getAnimalsOlderThan = (animal, age) => {
-  const { residents } = species.find(({ name }) => name === animal);
-  return residents.every(({ age: acAge }) => acAge >= age);
-};
+const getAnimalsOlderThan = (animal, age) =>
+  species.find(({ name }) => name === animal).residents.every(({ age: acAge }) => acAge >= age);
 
 module.exports = getAnimalsOlderThan;
