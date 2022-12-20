4d206ce068052a4c9a2745e59114f670aa9699de
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-20 15:35:56 -0300

    Requisito 4: Pequena alteração

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,6 +1,6 @@
 const { employees } = require('../data/zoo_data');
 
-const mesage = 'O id inserido não é de uma pessoa colaboradora gerente!';
+const message = 'O id inserido não é de uma pessoa colaboradora gerente!';
 
 const checkManager = (allManagers, managers) => managers.some((m) => allManagers.includes(m));
 
@@ -11,7 +11,7 @@ const getAllManagers = () =>
 const isManager = (id) => getAllManagers().includes(id);
 
 const getRelatedEmployees = (managerId) => {
-  if (!isManager(managerId)) throw new Error(mesage);
+  if (!isManager(managerId)) throw new Error(message);
 
   return employees.reduce((acc, { managers, firstName, lastName }) =>
     (managers.includes(managerId) ? [...acc, `${firstName} ${lastName}`] : acc), []);
