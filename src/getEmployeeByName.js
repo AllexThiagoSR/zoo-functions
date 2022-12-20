bdfc57c62afdf4c48fa2376a7af87c030e7c5e22
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-20 14:05:26 -0300

    Requsito 3: Pequenas alterações

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,9 +1,7 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const { employees } = data;
-
-const getEmployeeByName = (employeeName) => employees.find(({ firstName, lastName }) => (
-  firstName === employeeName || lastName === employeeName
-)) || {};
+const getEmployeeByName = (employeeName) =>
+  employees.find(({ firstName, lastName }) =>
+    (firstName === employeeName || lastName === employeeName)) || {};
 
 module.exports = getEmployeeByName;
