52642cdb881ac11d0a6b0e23aae12d8f930f4fc6
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-20 15:31:00 -0300

    Requisito 4: Algumas pequenas alterações

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,16 +1,20 @@
 const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => employees.reduce((allManagers, { managers }) => (
-  allManagers.includes(id) ? allManagers : [...allManagers, ...managers]
-), []).includes(id);
+const mesage = 'O id inserido não é de uma pessoa colaboradora gerente!';
+
+const checkManager = (allManagers, managers) => managers.some((m) => allManagers.includes(m));
+
+const getAllManagers = () =>
+  employees.reduce((allManagers, { managers }) =>
+    (checkManager(allManagers, managers) ? allManagers : [...allManagers, ...managers]), []);
+
+const isManager = (id) => getAllManagers().includes(id);
 
 const getRelatedEmployees = (managerId) => {
-  if (isManager(managerId)) {
-    return employees.reduce((acc, { managers, firstName, lastName }) => (
-      managers.includes(managerId) ? [...acc, `${firstName} ${lastName}`] : acc
-    ), []);
-  }
-  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  if (!isManager(managerId)) throw new Error(mesage);
+
+  return employees.reduce((acc, { managers, firstName, lastName }) =>
+    (managers.includes(managerId) ? [...acc, `${firstName} ${lastName}`] : acc), []);
 };
 
 module.exports = { isManager, getRelatedEmployees };
