4837730a3eee8f5058f911f1f10007bb42c863ac
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-07 12:01:54 -0300

    Requisito 8: Muda a função do reduce para um função externa

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,16 +2,18 @@ const data = require('../data/zoo_data');
 
 const { prices } = data;
 
-const creatObject = (array) =>
-  Object.keys(array).reduce((finaObj, currKey) => ({ ...finaObj, [currKey]: 0 }), {});
+const creatObject = (obj) =>
+  Object.keys(obj).reduce((finaObj, currKey) => ({ ...finaObj, [currKey]: 0 }), {});
 
-const countEntrants = (entrants) => entrants.reduce((count, { age }) => {
-  let { child, adult, senior } = count;
+const addOneByAge = (entrants, { age }) => {
+  let { child, adult, senior } = entrants;
   if (age < 18) child += 1;
   else if (age >= 18 && age < 50) adult += 1;
   else senior += 1;
   return { child, adult, senior };
-}, creatObject(prices));
+};
+
+const countEntrants = (entrants) => entrants.reduce(addOneByAge, creatObject(prices));
 
 const calculateEntry = (entrants) => {
   if (!Array.isArray(entrants) || entrants.length === 0) return 0;
